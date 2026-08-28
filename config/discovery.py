d8cf8401da8a1819a94e958fdd29e35fd25eda22
Michael Coutinho <michaelcoutyy@gmail.com>
2026-08-27 21:07:10 -0300

    code: add public discovery configuration

---
+++ config/discovery.py
@@ -0,0 +1,44 @@
+TAVILY_MONTHLY_BUDGET = 60
+
+TAVILY_MAX_RESULTS = 10
+
+
+DISCOVERY_SEARCHES = [
+
+    {
+        "name": "greenhouse",
+        "query":
+            "Brazil software developer data machine learning AI jobs careers",
+        "domains": [
+            "job-boards.greenhouse.io",
+            "boards.greenhouse.io",
+        ],
+    },
+
+    {
+        "name": "lever",
+        "query":
+            "Brazil software developer data machine learning AI jobs",
+        "domains": [
+            "jobs.lever.co",
+        ],
+    },
+
+    {
+        "name": "ashby",
+        "query":
+            "Brazil software engineer developer data AI jobs",
+        "domains": [
+            "jobs.ashbyhq.com",
+        ],
+    },
+
+    {
+        "name": "workable",
+        "query":
+            "Brazil software developer data AI jobs",
+        "domains": [
+            "apply.workable.com",
+        ],
+    },
+]
